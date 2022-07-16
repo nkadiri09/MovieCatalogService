0171f69d9b504a57b989aa05b38fa3771b1ceeab
Narendra <nkadiri09@gmail.com>
2022-07-16 17:23:19 -0400

    Added Spring discovery

--- src/test/java/com/naren/career/moviecatalogservice/MovieCatalogServiceApplicationTests.java
+++ src/test/java/com/naren/career/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -1,12 +1,11 @@
 package com.naren.career.moviecatalogservice;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MovieCatalogServiceApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
